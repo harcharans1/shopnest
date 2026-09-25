5c579e4773de8ea709c5616d71b757d0b76acaab
harcharans1 <162577124+harcharans1@users.noreply.github.com>
2026-09-25 13:37:33 +0530

    Fix duplicate products route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,56 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
     path: '',
     loadComponent: () =>
       import('./features/home/home')
         .then(m => m.Home)
   },
-
   {
     path: 'products',
     loadComponent: () =>
       import('./features/products/products')
         .then(m => m.Products)
   },
-
-  {
-    path: 'cart',
-    loadComponent: () =>
-      import('./features/cart/cart')
-        .then(m => m.Cart)
-  },
-
-  {
-    path: 'categories',
-    loadComponent: () =>
-      import('./features/categories/categories')
-        .then(m => m.Categories)
-  },
-
-  {
-    path: 'checkout',
-    loadComponent: () =>
-      import('./features/checkout/checkout')
-        .then(m => m.Checkout)
-  },
-
-  {
-    path: 'products',
-    loadComponent: () =>
-      import('./features/products/products')
-        .then(m => m.Products)
-  },
-
-{
-  path: 'wishlist',
-  loadComponent: () =>
-    import('./features/wishlist/wishlist')
-      .then(m => m.Wishlist)
-},
-
   {
     path: 'products/:id',
     loadComponent: () =>
@@ -58,8 +20,31 @@ export const routes: Routes = [
         .then(m => m.ProductDetails)
   },
   {
+    path: 'categories',
+    loadComponent: () =>
+      import('./features/categories/categories')
+        .then(m => m.Categories)
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./features/cart/cart')
+        .then(m => m.Cart)
+  },
+  {
+    path: 'wishlist',
+    loadComponent: () =>
+      import('./features/wishlist/wishlist')
+        .then(m => m.Wishlist)
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./features/checkout/checkout')
+        .then(m => m.Checkout)
+  },
+  {
     path: '**',
     redirectTo: ''
   }
-
 ];
