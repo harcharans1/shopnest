8d068478af0ed3efe77ccee54f52dd6138adb0bb
harcharans1 <162577124+harcharans1@users.noreply.github.com>
2026-09-25 13:52:36 +0530

    Improve cart lifecycle and clear confirmation

--- src/app/features/cart/cart.ts
+++ src/app/features/cart/cart.ts
@@ -1,79 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import {
-  CartService,
-  CartProduct
-} from '../../core/services/cart';
+import { Subscription } from 'rxjs';
+import { CartService, CartProduct } from '../../core/services/cart';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink
-  ],
+  imports: [CommonModule, RouterLink],
   templateUrl: './cart.html',
   styleUrl: './cart.scss'
 })
-export class Cart {
-
+export class Cart implements OnInit, OnDestroy {
   cart: CartProduct[] = [];
+  private cartSubscription?: Subscription;
 
+  constructor(private cartService: CartService) {}
 
-  constructor(
-    private cartService: CartService
-  ) {
-
-    this.cartService.cart$
-      .subscribe(items => {
-
-        this.cart = items;
-
-      });
-
+  ngOnInit(): void {
+    this.cartSubscription = this.cartService.cart$.subscribe(items => {
+      this.cart = items;
+    });
   }
-
 
   increase(id: number): void {
-
     this.cartService.increaseQuantity(id);
-
   }
-
 
   decrease(id: number): void {
-
     this.cartService.decreaseQuantity(id);
-
   }
-
 
   remove(id: number): void {
-
     this.cartService.removeFromCart(id);
-
   }
-
 
   clearCart(): void {
-
-    this.cartService.clearCart();
-
+    if (!this.cart.length) return;
+    const confirmed = confirm('Are you sure you want to clear your cart?');
+    if (confirmed) this.cartService.clearCart();
   }
-
 
   get subtotal(): number {
-
     return this.cartService.getTotalPrice();
-
   }
-
 
   get totalItems(): number {
-
     return this.cartService.getTotalItems();
-
   }
 
+  ngOnDestroy(): void {
+    this.cartSubscription?.unsubscribe();
+  }
 }
